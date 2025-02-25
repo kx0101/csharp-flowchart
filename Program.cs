efcdf08cc9d9c2d9b3f08e42c57681fe79e5f8cb
kx0101 <liakos.koulaxis@yahoo.com>
2025-02-25 13:24:50 +0200

    so good

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ public class Sample
 {
     public static void Main(string[] args)
     {
-        var y = 4 + 3;
-        Test(y);
+        var x = 4 + 3;
+        Test(x);
     }
 
     public void Test(int x)
     {
+        var z = 1;
+
         if (x > 5)
         {
             Console.WriteLine(""Greater"");
@@ -41,24 +43,37 @@ string flowchart = "digraph Flowchart {\n";
 
 int nodeId = 1;
 int parentId = 0;
+int lastMethodNodeId = 0;
 Dictionary<string, int> methodNodes = new();
 Dictionary<string, string> variables = new();
+Dictionary<string, List<string>> methodParameters = new();
 
 foreach (var method in root.DescendantNodes().OfType<MethodDeclarationSyntax>())
 {
     methodNodes[method.Identifier.Text] = nodeId;
+    var parameterNames = method.ParameterList.Parameters
+        .Select(p => p.Identifier.Text)
+        .ToList();
+    methodParameters[method.Identifier.Text] = parameterNames;
 
-    WriteLine(method.Identifier);
     if (nodeId == 1)
     {
         flowchart += $"    Node0 [shape=box, label=\"{method.Identifier.Text}()\"];\n";
+        lastMethodNodeId = 0;
     }
 
-    foreach (var statement in method.Body.Statements)
+    parentId = lastMethodNodeId;
+
+    for (int i = 0; i < method.Body.Statements.Count; i++)
     {
-        flowchart += ProcessStatement(statement, ref nodeId, $"Node{parentId}", methodNodes, variables);
-        parentId++;
+        var statement = method.Body.Statements[i];
+        var result = ProcessStatement(statement, ref nodeId, $"Node{parentId}", methodNodes, variables);
+        flowchart += result;
+
+        parentId = nodeId - 1;
     }
+
+    lastMethodNodeId = parentId;
 }
 
 flowchart += "}\n";
@@ -88,11 +103,6 @@ static string ProcessStatement(
         {
             int evaluatedValue = EvaluateBinaryExpression(binaryExpr);
             variables[variableName] = evaluatedValue.ToString();
-
-            output += $"    Node{currentId} [shape=ellipse, label=\"{variableName} = {varDecl.Declaration.Variables.First().Initializer.Value}\"];\n";
-            output += $"    {parentNode} -> Node{currentId};\n";
-
-            return output;
         }
 
         output += $"    Node{currentId} [shape=ellipse, label=\"{variableName} = {variableValue.Replace("\"", "\\\"")}\"];\n";
@@ -101,47 +111,97 @@ static string ProcessStatement(
         return output;
     }
 
-    if (statement is ExpressionStatementSyntax exprStmt)
+    if (statement is ExpressionStatementSyntax exprStmt &&
+        exprStmt.Expression is InvocationExpressionSyntax invocationExpr)
     {
-        if (exprStmt.Expression is InvocationExpressionSyntax invocationExpr)
+        var invocationText = invocationExpr.ToString();
+
+        foreach (var variable in variables)
         {
-            var invocationText = invocationExpr.ToString();
-            foreach (var variable in variables)
+            foreach (var arg in invocationExpr.ArgumentList.Arguments)
             {
-                if (invocationText.Contains($"{{{variable.Key}}}"))
+                if (arg.Expression is IdentifierNameSyntax argName &&
+                    argName.Identifier.Text == variable.Key)
                 {
-                    var rawValue = variable.Value.Trim('"');
-                    invocationText = invocationText.Replace($"{{{variable.Key}}}", rawValue);
-                }
-                else
-                {
-                    invocationText = invocationText.Replace(variable.Key, variable.Value);
+                    if (invocationText.Contains($"{{{variable.Key}}}"))
+                    {
+                        var rawValue = variable.Value.Trim('"');
+                        invocationText = invocationText.Replace($"{{{variable.Key}}}", rawValue);
+                    }
+                    else
+                    {
+                        invocationText = invocationText.Replace(variable.Key, variable.Value);
+                    }
                 }
             }
-
-            output += $"    Node{currentId} [shape=box, label=\"{invocationText.Replace("\"", "\\\"")}\"];\n";
-            output += $"    {parentNode} -> Node{currentId};\n";
-
-            return output;
         }
-    }
 
-    if (statement is BlockSyntax blockStmt)
-    {
-        foreach (var stmt in blockStmt.Statements)
+        foreach (var variable in variables)
         {
-            return ProcessStatement(stmt, ref nodeId, parentNode, methodNodes, variables);
+            if (invocationText.Contains($"{{{variable.Key}}}"))
+            {
+                var rawValue = variable.Value.Trim('"');
+                invocationText = invocationText.Replace($"{{{variable.Key}}}", rawValue);
+            }
         }
+
+        output += $"    Node{currentId} [shape=box, label=\"{invocationText.Replace("\"", "\\\"")}\"];\n";
+        output += $"    {parentNode} -> Node{currentId};\n";
+
+        return output;
     }
 
-    var statementText = statement.ToString();
-    foreach (var variable in variables)
+    if (statement is IfStatementSyntax ifStmt)
     {
-        statementText = statementText.Replace(variable.Key, variable.Value);
-    }
+        var conditionText = ifStmt.Condition.ToString();
+        output += $"    {parentNode} -> Node{currentId} [label=\"{conditionText}\"];\n";
 
-    output += $"    Node{currentId} [shape=box, label=\"{statementText.Replace("\"", "\\\"")}\"];\n";
-    output += $"    {parentNode} -> Node{currentId};\n";
+        foreach (var variable in variables)
+        {
+            conditionText = conditionText.Replace(variable.Key, variable.Value);
+        }
+
+        output += $"    Node{currentId} [shape=diamond, label=\"{conditionText}\"];\n";
+
+        int ifNodeId = currentId;
+        int lastNodeId = ifNodeId;
+
+        if (EvaluateCondition(conditionText))
+        {
+            if (ifStmt.Statement is BlockSyntax block)
+            {
+                foreach (var blockStatement in block.Statements)
+                {
+                    output += ProcessStatement(blockStatement, ref nodeId, $"Node{lastNodeId}", methodNodes, variables);
+                    lastNodeId = nodeId - 1;
+                }
+            }
+            else
+            {
+                output += ProcessStatement(ifStmt.Statement, ref nodeId, $"Node{ifNodeId}", methodNodes, variables);
+                lastNodeId = nodeId - 1;
+            }
+        }
+        else if (ifStmt?.Else != null)
+        {
+            if (ifStmt.Else.Statement is BlockSyntax block)
+            {
+                foreach (var blockStatement in block.Statements)
+                {
+                    output += ProcessStatement(blockStatement, ref nodeId, $"Node{lastNodeId}", methodNodes, variables);
+                    lastNodeId = nodeId - 1;
+                }
+            }
+            else
+            {
+                output += ProcessStatement(ifStmt.Else.Statement, ref nodeId, $"Node{ifNodeId}", methodNodes, variables);
+                lastNodeId = nodeId - 1;
+            }
+        }
+
+        nodeId = lastNodeId + 1;
+        return output;
+    }
 
     return output;
 }
@@ -161,6 +221,19 @@ static int EvaluateBinaryExpression(BinaryExpressionSyntax binaryExpr)
             _ => 0
         };
     }
-
     return 0;
+}
+
+static bool EvaluateCondition(string condition)
+{
+    try
+    {
+        var dataTable = new System.Data.DataTable();
+        return Convert.ToBoolean(dataTable.Compute(condition, ""));
+    }
+    catch
+    {
+        WriteLine("Condition evaluation failed.");
+        return false;
+    }
 }
